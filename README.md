e1f9e6f3728cea21d7d98e3ca565a9f40fed2558
Horia Constantin <horia.constantin@icloud.com>
2019-10-30 16:06:59 +0100

    added spring boot template

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # distanceConverter
+
+## Steps to solution
+1. Create spring boot project
+2. Implement distance converting logic
+3. Implement webservice
+4. Implement html frontend that uses webservice
+5. Serve html from spring app
+5. Dockerize service
---
+++ src/test/java/com/horiaconstantin/distanceConverter/DistanceConverterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.horiaconstantin.distanceConverter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistanceConverterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
