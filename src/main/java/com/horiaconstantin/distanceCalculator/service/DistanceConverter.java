2417443ae402473ea9b414f6960aaeebc0a8817b
Horia Constantin <horia.constantin@icloud.com>
2019-10-30 17:27:50 +0100

    added toYards implementation

--- src/main/java/com/horiaconstantin/distanceCalculator/service/DistanceConverter.java
+++ src/main/java/com/horiaconstantin/distanceCalculator/service/DistanceConverter.java
@@ -5,13 +5,14 @@ import lombok.experimental.UtilityClass;
 
 /**
  * Class that converts yards to meters and meters to yards.
- * The current implementation offers precision up to 2 decimals
+ * The current implementation offers precision up to 2 decimals.
+ * NOTE: The implementation uses double, therefore it's going to imprecise at big distance values
  */
 @UtilityClass
 class DistanceConverter {
 
     /**
-     * Convers distance expressed in yards to meters
+     * Converts a distance expressed in yards to meters
      * @return a distance measured in meters
      */
     Distance toMeters(@NonNull Distance distance){
@@ -19,6 +20,30 @@ class DistanceConverter {
         if (Unit.YARDS == distance.getUnit()) {
             valueInMeters = distance.getValue() / 1.0936;
         }
+
         return new Distance(valueInMeters, Unit.METERS);
+    }
+
+    /**
+     * Converts a distance expressed in meters to yards
+     *
+     * @return a distance measured in yards
+     * @throws ArithmeticException when converting the yards to meters exceeds Double.MAX_VALUE
+     */
+    Distance toYards(@NonNull Distance distance) {
+        var valueInYards = distance.getValue();
+        if (Unit.METERS == distance.getUnit()) {
+            valueInYards = distance.getValue() * 1.0936;
+        }
+
+        checkNumericOverflow(valueInYards);
+        return new Distance(valueInYards, Unit.YARDS);
+    }
+
+    private static void checkNumericOverflow(Double value) {
+        if (value == Double.POSITIVE_INFINITY) {
+            throw new ArithmeticException("Conversion of " + value + " to desired unit failed because it cause a " +
+                    "numeric overflow");
+        }
     }
 }
--- src/test/java/com/horiaconstantin/distanceCalculator/service/DistanceConverterTest.java
+++ src/test/java/com/horiaconstantin/distanceCalculator/service/DistanceConverterTest.java
@@ -7,13 +7,15 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class DistanceConverterTest {
 
+    private static final double PRECISION = 0.01;
+
     @Test
     void toMeters_nullNotAllowed() {
         assertThrows(NullPointerException.class, () -> DistanceConverter.toMeters(null));
     }
 
     @Test
-    void toMeters_distanceInMetersIsNotConverted() {
+    void toMeters_metersStayTheSame() {
         var distance = new Distance(100, Unit.METERS);
 
         var actual = DistanceConverter.toMeters(distance);
@@ -27,7 +29,7 @@ class DistanceConverterTest {
 
         var actual = DistanceConverter.toMeters(distance);
 
-        assertEquals(91.44, actual.getValue(), 0.009);
+        assertEquals(91.44, actual.getValue(), PRECISION);
     }
 
     @Test
@@ -36,6 +38,36 @@ class DistanceConverterTest {
 
         var actual = DistanceConverter.toMeters(distance);
 
-        assertEquals(Double.MAX_VALUE / 1.0936, actual.getValue(), 0.009);
+        assertEquals(Double.MAX_VALUE / 1.0936, actual.getValue(), PRECISION);
+    }
+
+    @Test
+    void toYards_nullNotAllowed() {
+        assertThrows(NullPointerException.class, () -> DistanceConverter.toYards(null));
+    }
+
+    @Test
+    void toYards_yardsStayTheSame() {
+        var distance = new Distance(100, Unit.YARDS);
+
+        var actual = DistanceConverter.toYards(distance);
+
+        assertEquals(distance, actual);
+    }
+
+    @Test
+    void toYards_convertDistanceInMeters() {
+        var distance = new Distance(100, Unit.METERS);
+
+        var actual = DistanceConverter.toYards(distance);
+
+        assertEquals(109.35, actual.getValue(), PRECISION);
+    }
+
+    @Test
+    void toYards_convertDoubleMAX() {
+        var distance = new Distance(Double.MAX_VALUE, Unit.METERS);
+
+        assertThrows(ArithmeticException.class, () -> DistanceConverter.toYards(distance));
     }
 }
